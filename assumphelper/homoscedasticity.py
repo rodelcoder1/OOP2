f17dcdf32a3266823a47fd0a7ccfce5d6b346f27
rodelcoder1 <rodelpellazar1@gmail.com>
2025-12-19 02:07:17 +0800

    Update homoscedasticity.py

--- assumphelper/homoscedasticity.py
+++ assumphelper/homoscedasticity.py
@@ -23,7 +23,7 @@ class Homoscedasticity(Hypothesis):
         if not getattr(self, "fit_done", False):
             raise NotFittedError("Call fit() before test().")
              
-        check_residual_var(self.residuals)
+        check_resid_var(self.residuals)
         bp_test = het_breuschpagan(self.residuals, self.x_cons)
         bp_stat, bp_pval, _, _ = bp_test
         self.result = bp_pval
